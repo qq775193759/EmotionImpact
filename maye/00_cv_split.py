a44f520972df6e8b003c8480a572668843458de4
maye9999 <maye9999@163.com>
2016-06-28 19:28:54 +0800

    add a file

--- maye/00_cv_split.py
+++ maye/00_cv_split.py
@@ -1,1 +1,15 @@
-import sklearn
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('ACCEDEdescription.xml')
+root = tree.getroot()
+
+l = []
+for child in root:
+    d = {}
+    d['id'] = child[0].text
+    d['name'] = child[1].text
+    d['movie'] = child[3].text
+    l.append(d)
+
+
+print(d)
