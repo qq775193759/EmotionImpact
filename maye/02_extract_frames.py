d19ca56c230e8560c5e9bba07fb9c48bd5d6eb0e
maye9999 <maye9999@163.com>
2016-06-30 11:12:53 +0800

    c

--- maye/02_extract_frames.py
+++ maye/02_extract_frames.py
@@ -1,10 +1,13 @@
 import cv2
 from os import mkdir
+import multiprocessing
 
 N_MOVIES = 9800
 in_dir = "/media/hcsi3/EmotionImpact/2016data/data/"
 out_dir = "/media/hcsi3/EmotionImpact/2016data/frames/"
-for i in range(N_MOVIES):
+
+
+def process(i):
     filename = in_dir + "ACCEDE" + str(i).zfill(5) + ".mp4"
     mkdir(out_dir + str(i))
     out = out_dir + str(i) + "/"
@@ -15,7 +18,15 @@ for i in range(N_MOVIES):
     while success:
         success, image = video.read()
         if not success:
-            print(str(i) + "Done!")
+            print(str(i) + "\t\t\tDone!")
             break
         cv2.imwrite(out + "frame" + str(count) + ".png", image)
         count += 1
+
+
+if __name__ == "__main__":
+    pool = multiprocessing.Pool(processes=10)
+    for i in range(N_MOVIES):
+        pool.apply_async(process, (i, ))
+    pool.close()
+    pool.join()
