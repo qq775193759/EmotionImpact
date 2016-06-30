83082a489811216f5502afa20542df0bb0645347
maye9999 <maye9999@163.com>
2016-06-30 11:02:01 +0800

    a

---
+++ maye/02_extract_frames.py
@@ -0,0 +1,18 @@
+import cv2
+from os import mkdir
+
+N_MOVIES = 9800
+in_dir = "/media/hcsi3/EmotionImpact/2016data/data/"
+out_dir = "/media/hcsi3/EmotionImpact/2016data/frames/"
+for i in range(N_MOVIES):
+    filename = in_dir + "ACCEDE" + str(i).zfill(5) + ".mp4"
+    mkdir(out_dir + str(i))
+    out = out_dir + str(i) + "/"
+    video = cv2.VideoCapture(filename)
+    success, image = video.read()
+    count = 0
+    success = True
+    while success:
+        success, image = video.read()
+        cv2.imwrite(out + "frame" + str(count) + ".png", image)
+        count += 1
